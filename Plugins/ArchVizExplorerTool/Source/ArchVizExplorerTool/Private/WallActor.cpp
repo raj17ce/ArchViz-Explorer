0167bea45178c666af69552262d70f6d6caeff00
Raj Patel <raj17ce@gmail.com>
2024-06-19 14:32:08 +0530

    Create WallActor

---
+++ Plugins/ArchVizExplorerTool/Source/ArchVizExplorerTool/Private/WallActor.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WallActor.h"
+
+// Sets default values
+AWallActor::AWallActor() : WallSegmentIndex{ -1 }, WallSegmentRotation{ 0.0 }, PreviewWallSegment{nullptr} {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Component"));
+	SetRootComponent(SceneComponent);
+}
+
+// Called when the game starts or when spawned
+void AWallActor::BeginPlay() {
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AWallActor::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
+int32 AWallActor::GetSegmentIndex() const {
+	return WallSegmentIndex;
+}
+
+void AWallActor::SetSegmentIndex(int32 Index) {
+	WallSegmentIndex = Index;
+}
+
+FRotator AWallActor::GetSegmentRotation() const {
+	return WallSegmentRotation;
+}
+
+void AWallActor::SetSegmentRotation(const FRotator& Rotation) {
+	WallSegmentRotation = Rotation;
+}
---
+++ Plugins/ArchVizExplorerTool/Source/ArchVizExplorerTool/Public/WallActor.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WallActor.generated.h"
+
+UCLASS()
+class ARCHVIZEXPLORERTOOL_API AWallActor : public AActor {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWallActor();
+
+	int32 GetSegmentIndex() const;
+	void SetSegmentIndex(int32 Index);
+
+	FRotator GetSegmentRotation() const;
+	void SetSegmentRotation(const FRotator& Rotation);
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Wall")
+	UStaticMeshComponent* PreviewWallSegment;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Wall")
+	TArray<UStaticMeshComponent*> WallSegments;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Wall")
+	UStaticMesh* WallStaticMesh;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	USceneComponent* SceneComponent;
+
+private:
+	int32 WallSegmentIndex;
+	FRotator WallSegmentRotation;
+};
